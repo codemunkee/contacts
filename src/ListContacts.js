70451d5181a65d9bb90615f1a6a92c90a723b766
Russ Nealis <codemunkee@gmail.com>
2017-12-20 22:08:44 -0800

    added destructuring to clean up hte code a bit

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -20,12 +20,15 @@ class ListContacts extends Component {
 
   render() {
 
+    const { contacts, onDeleteContact } = this.props;
+    const { query } = this.state;
+
     let showingContacts;
-    if (this.state.query) {
-      const match = new RegExp(escapeRegExp(this.state.query, 'i'))
-      showingContacts = this.props.contacts.filter((c) => match.test(c.name));
+    if (query) {
+      const match = new RegExp(escapeRegExp(query, 'i'));
+      showingContacts = contacts.filter((c) => match.test(c.name));
     } else {
-      showingContacts = this.props.contacts;
+      showingContacts = contacts;
     }
 
     showingContacts.sort(sortBy('name'));
@@ -54,7 +57,7 @@ class ListContacts extends Component {
                 <p>{contact.name}</p>
                 <p>{contact.email}</p>
               </div>
-              <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+              <button onClick={() => onDeleteContact(contact)} className='contact-remove'>
                 Remove
               </button>
             </li>
