4bcb2c32bc09193ae801b3c92436f20f4db4dd79
Russ Nealis <codemunkee@gmail.com>
2017-12-21 17:16:03 -0800

    now remove via the API as well

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ class App extends Component {
   removeContact = (contact) => {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
-    }))
+    }));
+
+    ContactsAPI.remove(contact);
   };
 
   render() {
