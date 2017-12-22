f9a9a307d368374819e521f2f0dd77171d7b5118
Russ Nealis <codemunkee@gmail.com>
2017-12-22 07:13:53 -0800

    Routing for main app and /create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    screen: 'list', // list, create
     contacts: []
   };
 
@@ -26,21 +26,17 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        {this.state.screen === 'list' && (
+        <Route path="/" render={() => (
           <ListContacts
             contacts={this.state.contacts}
             onDeleteContact={this.removeContact}
-            onNavigate={() => {
-              this.setState({ screen: 'create' })
-            }}
           />
-        )}
-        {this.state.screen === 'create' && (
-          <CreateContact />
-        )}
+        )}/>
+        <Route path="/create" component={CreateContact} />
       </div>
     )
   }
 }
 
 export default App;
+
